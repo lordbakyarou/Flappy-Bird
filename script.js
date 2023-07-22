ec141a7a6d9a6d21a99561e5ebc29001f1a7f891
Mayur Hanwate <frostblizard111@gmail.com>
2023-07-23 00:49:54 +0530

    v2 flappy bird added with 2 blocks top and bottom

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 let block = document.getElementById("block");
-let hole = document.getElementById("hole");
+let block2 = document.getElementById("block2");
+
 let bird = document.getElementById("bird");
 let jumping = 0;
 let counter = 0;
 
-hole.addEventListener("animationiteration", () => {
-  let random = -(Math.random() * 300 + 150);
-  hole.style.top = random + "px";
+block.addEventListener("animationiteration", () => {
+  let minHeight = 20;
+  let maxHeight = 70;
+  let randomHeight1 = randomHeight(minHeight, maxHeight);
+
+  // Calculate the maximum possible height for block2 to maintain the 30% difference
+  let maxHeightBlock2 = 100 - randomHeight1 - 30;
+  let randomHeight2 = randomHeight(50, maxHeightBlock2); // Generate random height for block2 between 50% and maxHeightBlock2
+
+  block.style.height = randomHeight1 + "%";
+  block2.style.height = randomHeight2 + "%";
   counter++;
 });
+
+function randomHeight(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
 document.addEventListener("keydown", function (event) {
   if (event.code === "Space") {
@@ -22,23 +35,50 @@ setInterval(function () {
     bird.style.top = birdTop + 3 + "px";
   }
 
-  let blockLeft = parseInt(
+  let block1Left = parseInt(
     window.getComputedStyle(block).getPropertyValue("left")
   );
-  let holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+  let block2Left = parseInt(
+    window.getComputedStyle(block2).getPropertyValue("left")
+  );
 
-  let cTop = -(window.innerHeight - birdTop);
+  let block1Top = parseInt(
+    window.getComputedStyle(block).getPropertyValue("top")
+  );
+  let block2Top = parseInt(
+    window.getComputedStyle(block2).getPropertyValue("top")
+  );
+
+  let birdBottom = birdTop + bird.offsetHeight;
+
+  // Check collision with block1
   if (
-    birdTop > window.innerHeight - bird.offsetHeight ||
-    (blockLeft < bird.offsetWidth &&
-      blockLeft > -block.offsetWidth &&
-      (cTop < holeTop || cTop > holeTop + hole.offsetHeight))
+    birdTop < block1Top + block.offsetHeight &&
+    birdBottom > block1Top &&
+    block1Left < bird.offsetWidth
   ) {
-    alert("Game Over, Score: " + (counter - 1));
-    bird.style.top = 100 + "px";
-    counter = 0;
+    gameOver();
+  }
+
+  // Check collision with block2
+  if (
+    birdTop < block2Top + block2.offsetHeight &&
+    birdBottom > block2Top &&
+    block2Left < bird.offsetWidth
+  ) {
+    gameOver();
+  }
+
+  if (birdTop > window.innerHeight - bird.offsetHeight) {
+    gameOver();
   }
 }, 10);
+
+function gameOver() {
+  alert("Game Over, Score: " + (counter - 1));
+  bird.style.top = 100 + "px";
+  counter = 0;
+}
 
 function jump() {
   if (jumping === 0) {
